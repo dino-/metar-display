3bd92f32b40b9f8cf11b02958a98010c1553241d
Dino Morelli <dino@ui3.info>
2023-12-26 22:39:48 -0500

    Put METAR parsing code into a new module

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -3,8 +3,8 @@ import Network.Curl (CurlCode (CurlOK), curlGetString)
 import System.Exit (exitFailure, exitSuccess)
 import System.IO (hPutStrLn, stderr)
 import Text.Printf (printf)
-import Text.Regex (matchRegex, mkRegex)
 
+import PbMetar.Metar (parse)
 import PbMetar.Types
 
 
@@ -15,7 +15,7 @@ main = do
   let url = printf "%s/%s.TXT" urlPrefix station
   result <- resultToEither <$> curlGetString url []
   -- traceM . show $ result
-  let parsed = parseResult =<< result
+  let parsed = parse =<< result
   either exitFail exitOk parsed
 
 
@@ -57,29 +57,3 @@ calculateWindChill :: WindMph -> TempFahr -> TempFahr
 calculateWindChill (WindMph wind) (TempFahr tempF) = TempFahr $
   34.74 + (0.6215 * tempF) - (35.75 * (wind ** exp')) + (0.4275 * tempF * (wind ** exp'))
   where exp' = 0.16 :: Double
-
-
--- KRDU 261451Z 09008KT 10SM FEW070 BKN095 BKN110 OVC250 14/08 A3019 RMK AO2 SLP220 T01390083 53003
-
-parseResult :: String -> Either String Metar
-parseResult s = do
-  let metarString = head . take 1 . drop 1 . lines $ s
-
-  let parsedTime = matchRegex (mkRegex ".* [0-9]{2}([0-9]{2})([0-9]{2})Z .*") metarString
-  timeValues <- maybe (Left $ printf "Unable to parse time from: %s" metarString) Right parsedTime
-  let time' = Time (read $ timeValues !! 0) (read $ timeValues !! 1)
-
-  let parsedWind = matchRegex (mkRegex ".* [0-9]{3}([0-9]{2})KT .*") metarString
-  windValues <- maybe (Left $ printf "Unable to parse wind from: %s" metarString) Right parsedWind
-  let wind' = WindKts . read $ windValues !! 0
-
-  let mbTemp = mkTempCelsius $ matchRegex (mkRegex ".* T([0-9]{1})([0-9]{3})[0-9]{4}.*") metarString
-  temperature' <- maybe (Left $ printf "Unable to parse temperature from: %s" metarString) Right mbTemp
-
-  pure $ Metar time' wind' temperature'
-
-
-mkTempCelsius :: Maybe [String] -> Maybe TempCelsius
-mkTempCelsius (Just ["0", tempC]) = Just . TempCelsius . (/ 10.0) . read $ tempC
-mkTempCelsius (Just ["1", tempC]) = Just . TempCelsius . negate . (/ 10.0) . read $ tempC
-mkTempCelsius _ = Nothing
---
+++ src/lib/PbMetar/Metar.hs
@@ -0,0 +1,35 @@
+module PbMetar.Metar
+  ( parse
+  )
+  where
+
+import Text.Printf (printf)
+import Text.Regex (matchRegex, mkRegex)
+
+import PbMetar.Types
+
+
+-- KRDU 261451Z 09008KT 10SM FEW070 BKN095 BKN110 OVC250 14/08 A3019 RMK AO2 SLP220 T01390083 53003
+
+parse :: String -> Either String Metar
+parse s = do
+  let metarString = head . take 1 . drop 1 . lines $ s
+
+  let parsedTime = matchRegex (mkRegex ".* [0-9]{2}([0-9]{2})([0-9]{2})Z .*") metarString
+  timeValues <- maybe (Left $ printf "Unable to parse time from: %s" metarString) Right parsedTime
+  let time' = Time (read $ timeValues !! 0) (read $ timeValues !! 1)
+
+  let parsedWind = matchRegex (mkRegex ".* [0-9]{3}([0-9]{2})KT .*") metarString
+  windValues <- maybe (Left $ printf "Unable to parse wind from: %s" metarString) Right parsedWind
+  let wind' = WindKts . read $ windValues !! 0
+
+  let mbTemp = mkTempCelsius $ matchRegex (mkRegex ".* T([0-9]{1})([0-9]{3})[0-9]{4}.*") metarString
+  temperature' <- maybe (Left $ printf "Unable to parse temperature from: %s" metarString) Right mbTemp
+
+  pure $ Metar time' wind' temperature'
+
+
+mkTempCelsius :: Maybe [String] -> Maybe TempCelsius
+mkTempCelsius (Just ["0", tempC]) = Just . TempCelsius . (/ 10.0) . read $ tempC
+mkTempCelsius (Just ["1", tempC]) = Just . TempCelsius . negate . (/ 10.0) . read $ tempC
+mkTempCelsius _ = Nothing
