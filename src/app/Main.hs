3c5c763f929bc9b69cb204c63b69e86a906816c9
Dino Morelli <dino@ui3.info>
2024-01-01 12:01:28 -0500

    Separated output formatting code into a new module
    Closes gh-1

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,14 +1,12 @@
-{-# LANGUAGE OverloadedRecordDot #-}
-
-import Data.Time.LocalTime (TimeOfDay (..), getCurrentTimeZone)
+import Data.Time.LocalTime (getCurrentTimeZone)
 import System.Exit (exitFailure, exitSuccess)
-import Text.Printf (printf)
 
-import PbMetar.Common
+import PbMetar.Common (Options (..))
 import PbMetar.Curl (getMetar)
 import PbMetar.Log (criticalM, infoM, initLogging, lname, noticeM, out)
 import PbMetar.Metar (isolateMetarLine, parse)
 import PbMetar.Opts (parseOpts)
+import PbMetar.Output (mkPolybarLabel)
 
 
 main :: IO ()
@@ -21,39 +19,19 @@ main = do
   either (const $ pure ()) (noticeM lname) eMetarString
   tz <- getCurrentTimeZone
   let parsed = parse tz =<< eMetarString
-  either exitFail (exitOk (optFontIndex opts) (optColorText opts)) parsed
+  either exitFail (exitOk . mkPolybarLabel (optFontIndex opts) (optColorText opts)) parsed
 
 
-exitOk :: FontIndex -> ColorText -> Weather -> IO ()
-exitOk fi@(FontIndex fontIndex) colorText (Weather obs chill) = do
-  let (TempFahr tempF) = obs.tempF
-  let (TempCelsius tempC) = obs.tempC
-  let (WindMph windMph) = obs.windMph
-  let (colorBegin, colorEnd) = mkColorFormatting colorText
-  let outputString = printf "%%{T%d}\xe586%%{T-} %s%02d:%02d%s %%{T%d}\xf2c9%%{T-} %s%.0f°F %01f°C%s %%{T%d}\xf72e%%{T-} %s%.1fmph%s%s"
-        fontIndex colorBegin obs.time.todHour obs.time.todMin colorEnd
-        fontIndex colorBegin tempF tempC colorEnd
-        fontIndex colorBegin windMph colorEnd (mkWindChillDisplay fi colorText chill)
-  infoM lname $ "stdout output string: " <> outputString
-  out outputString
+exitOk :: String -> IO ()
+exitOk polybarLabel = do
+  infoM lname $ "stdout output string: " <> polybarLabel
+  out polybarLabel
   infoM lname "polybar-metar-weather finished successfully"
   exitSuccess
 
 
-mkWindChillDisplay :: FontIndex -> ColorText -> WindChill -> String
-mkWindChillDisplay (FontIndex fontIndex) colorText (WindChill (TempCelsius windChillC) (TempFahr windChillF)) =
-  printf " %%{T%d}\xf7ad%%{T-} %s%.0f°F %.1f°C%s" fontIndex colorBegin windChillF windChillC colorEnd
-  where (colorBegin, colorEnd) = mkColorFormatting colorText
-mkWindChillDisplay _ _ NoEffect = ""
-
-
-mkColorFormatting :: ColorText -> (String, String)
-mkColorFormatting NoColorChange = ("", "")
-mkColorFormatting (ColorText colText) = ( printf "%%{F%s}" colText, "%%{F-}")
-
-
 exitFail :: String -> IO ()
 exitFail errorInfo = do
-  criticalM lname $ printf "Error: %s" errorInfo
+  criticalM lname $ "Error: " <> errorInfo
   out "ERROR! See log for more info"
   exitFailure
---
+++ src/lib/PbMetar/Output.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
+module PbMetar.Output
+  ( mkPolybarLabel
+  )
+  where
+
+import Data.Time.LocalTime (TimeOfDay (..))
+import Text.Printf (printf)
+
+import PbMetar.Common
+  ( ColorText (NoColorChange, ColorText)
+  , FontIndex (..)
+  , Observations (..)
+  , TempCelsius (..)
+  , TempFahr (..)
+  , Weather (..)
+  , WindChill (NoEffect, WindChill)
+  , WindMph (..)
+  )
+
+
+mkPolybarLabel :: FontIndex -> ColorText -> Weather -> String
+mkPolybarLabel fi@(FontIndex fontIndex) colorText (Weather obs chill) = do
+  let (TempFahr tempF) = obs.tempF
+  let (TempCelsius tempC) = obs.tempC
+  let (WindMph windMph) = obs.windMph
+  let (colorBegin, colorEnd) = mkColorFormatting colorText
+  printf "%%{T%d}\xe586%%{T-} %s%02d:%02d%s %%{T%d}\xf2c9%%{T-} %s%.0f°F %01f°C%s %%{T%d}\xf72e%%{T-} %s%.1fmph%s%s"
+        fontIndex colorBegin obs.time.todHour obs.time.todMin colorEnd
+        fontIndex colorBegin tempF tempC colorEnd
+        fontIndex colorBegin windMph colorEnd (mkWindChillDisplay fi colorText chill)
+
+
+mkWindChillDisplay :: FontIndex -> ColorText -> WindChill -> String
+mkWindChillDisplay (FontIndex fontIndex) colorText (WindChill (TempCelsius windChillC) (TempFahr windChillF)) =
+  printf " %%{T%d}\xf7ad%%{T-} %s%.0f°F %.1f°C%s" fontIndex colorBegin windChillF windChillC colorEnd
+  where (colorBegin, colorEnd) = mkColorFormatting colorText
+mkWindChillDisplay _ _ NoEffect = ""
+
+
+mkColorFormatting :: ColorText -> (String, String)
+mkColorFormatting NoColorChange = ("", "")
+mkColorFormatting (ColorText colText) = ( printf "%%{F%s}" colText, "%%{F-}")
